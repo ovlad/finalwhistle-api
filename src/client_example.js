263cb0ae06907de570c60b741d05b436891d15a7
Vlad ONCIOIU <voncioiu@bitdefender.com>
2018-10-20 19:05:46 +0300

    Fix.

--- src/client_example.js
+++ src/client_example.js
@@ -1,7 +1,8 @@
 const socketIO = require('socket.io-client');
 const socket = socketIO('https://finalwhistle-api.herokuapp.com');
 
-// prints whatever data the server sends through the 'hello' event
+// any time the server sends data through the 'hello' event,
+// the client prints it
 socket.on('hello', data => {
     console.log(data);
 });
